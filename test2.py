bf3a6a9d861748929e57f395217306ac3dc37179
MartinMashalov <52296303+MartinMashalov@users.noreply.github.com>
2020-08-19 10:24:11 -0400

    Finished dialog

--- test2.py
+++ test2.py
@@ -1,25 +1,25 @@
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 from test1 import send_message
-import findCounty as fc
 import time
-import requests
+import findCounty as fc
+from nearestCentersDictionary import nearest_centers
+from test import formulate_message
 import urllib.request
 import os, ssl
 import random
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
 
+
+
 app = Flask(__name__)
 account_sid = 'AC0d79e56293d4494c36eee4f48a59ff8e'
-auth_token = '6120cce02a157c3a76eff6ca72d98d76'
+auth_token = '0564dd82c87dedf2c130e8db7f5244a8'
 client = Client(account_sid, auth_token)
-#print(client)
 bot_number = "+14155238886"
 
 user_zips = {}
-
 myths = [
     'Covid-19 is not waterborne; however, it is aerosolic and airborne',
     'Covid-19 is not created in a lab as conspiracy theorists would say',
@@ -35,57 +35,87 @@ def hello():
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    #print("hello from top")
     msg = request.form.get('Body')
-    last_msg = msg
     phone_no = request.form.get('From')
     responded = False
 
     if msg.lower() == "Hi".lower() or msg.lower() == "hello".lower():
-        send_message("Hello there", bot_number, phone_no)
+        send_message("Hi There! I'm CovidBot. I'm here to help you navigate your way through this pandemic safely.", bot_number, phone_no)
+        time.sleep(1)
         responded = send_menu(bot_number, phone_no)
 
-    if msg.lower() == "joke".lower():
+    elif msg.lower() == "joke".lower():
         send_message("Hippos are blue?", bot_number, phone_no)
 
     #latest numbers
-    if msg == 1:
-        send_message("Supply a zip code:", bot_number, phone_no)
+    elif msg == 'A':
+        if phone_no in user_zips.keys():
+            zip = user_zips[phone_no][0]
+            county = fc.find_county_by_zip_code(zip)
+            if county[1] == "No":
+               send_message("Enter a valid zip code. Otherwise, I'm sorry, I cannot retrieve data for you :(", bot_number, phone_no)
+            elif county[0] == 2:
+                send_message("Which county are you located in: {}".format(county[1]), bot_number, phone_no)
+            # length 1, no need for more user input
+            else:
+                send_message("The number of cases are: (Westchester not complete)  ", bot_number, phone_no)
+        else:
+            send_message("Supply a zip code:", bot_number, phone_no)
 
-    #process zip_code, input in user_data zip_code, county, a nearest test centers array
-    if type(msg) is int and len(msg) == 5:
-        county = fc.find_county_by_zip_code(int(msg))
-        if county == "No":
-            send_message("Enter a valid zip code. Otherwise, I'm sorry, I cannot retrieve data for you :(", bot_number, phone_no)
-        else: 
-            # store zip codes for user
-            user_zips[phone_no] = [int(msg), county]
-            #send it
-            send_message("The number of cases are:   ", bot_number, phone_no)
+    #store zip_code (OPERATIONAL)
+    elif len(list(msg)) == 5:
+        #make sure there are no duplicates or if the first time, there is an incorrect value, get rid of it to store new value
+        print(user_zips)
+        if phone_no not in user_zips.keys():
+            user_zips[phone_no] = [int(msg)]
+        else:
+            del user_zips[phone_no]
+            user_zips[phone_no] = [int(msg)]
+        send_message("Got it! Re-enter the letter you type before.", bot_number, phone_no)
+        print(user_zips)
 
-    #myths debunked
-    if msg == 3:
+    #myths debunked (OPERATIONAL)
+    elif msg.lower() == 'B'.lower():
         sent = random.choice(myths)
         send_message(sent, bot_number, phone_no)
 
-    if msg == 4:
-        pass
+    #nearest test centers
+    elif msg.lower() == 'C'.lower():
+        if phone_no in user_zips.keys():
+            search = str(user_zips[phone_no][0]) + ", New York"
+            centers = nearest_centers[search]
 
+            print(centers)
+
+            send_message(formulate_message(centers[0][1]), bot_number, phone_no)
+            time.sleep(1)
+            '''
+            send_message(formulate_message(centers[1][1]), bot_number, phone_no)
+            time.sleep(1)
+            send_message(formulate_message(centers[2][1]), bot_number, phone_no)
+            time.sleep(1)
+            send_message(formulate_message(centers[3][1]), bot_number, phone_no)'''
+        else:
+            send_message("Please provide a zip code and re-enter the letter you typed:  ", bot_number, phone_no)
+
+    else:
+        send_message("I don't understand. Here is the menu again: ", bot_number, phone_no)
+        responded = send_menu(bot_number, phone_no)
 
     return "ok"
 
+'''
 def download_csv():
     url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-15-2020.csv"
     print("download start!")
     filename, headers = urllib.request.urlretrieve(url, filename="/Users/martinmashalov/Documents/Python/twilioTest2/CovidData.csv")
     print("download complete!")
-    print("download file location: ", filename)
-
-#download_csv()
-#download_csv("https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports_us/08-15-2020.csv")
+    print("download file location: ", filename)'''
 
 def send_menu(bot_num, phone_no):
-    keyword = 'Joke\nTestItem'
+    keyword = "Type 'A' ->  Get Info on Cases\n" \
+              "Type 'B' ->  Debunk those myths!\n" \
+              "Type 'C' ->  Finds the nearest Covid-19 testing centers"
     send_message(keyword, bot_num, phone_no)
     return True
 
