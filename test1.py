de24b9f4ef6e824697ca2650ac67a7b57f11c1bc
MartinMashalov <52296303+MartinMashalov@users.noreply.github.com>
2020-09-11 20:05:01 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC0d79e56293d4494c36eee4f48a59ff8e'
-auth_token = 'e28b70572daa7f80fe2bccb3b7357432'
+auth_token = 'add1554bab0e843fabc0cf9bfcf4c99c'
 client = Client(account_sid, auth_token)
 
 
