d9a7616e537e40270553e6f725ded3116d0414e7
MartinMashalov <52296303+MartinMashalov@users.noreply.github.com>
2020-09-16 22:14:00 -0400

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,19 +1,20 @@
 from twilio.rest import Client
+from app import auth_token
 
-#auth_token = AccessToken('AC0d79e56293d4494c36eee4f48a59ff8e', 'SKeb5a25313bb48bd91b84eedbe6834520', 'bsamiAWrhwdHLCYpPNiy6Ch2t9WT1gAT', identity='PythonCovidBot')
 account_sid = 'AC0d79e56293d4494c36eee4f48a59ff8e'
-auth_token = 'add1554bab0e843fabc0cf9bfcf4c99c'
+#auth_token = 'e28b70572daa7f80fe2bccb3b7357432'
 client = Client(account_sid, auth_token)
 
+
 def send_message(message_, from_, to):
-    print('whatsappFROM:' + from_)
-    print('whatsappTO:' + to)
+    print('whatsappFROM: ' + from_)
+    print('whatsappTO: ' + to)
 
     if 'w' in list(to):
         message = client.messages \
             .create(
             body=message_,
-            from_="whatsapp:" + from_,
+            from_="whatsapp:"+ from_,
             to=to
         )
     else:
@@ -32,4 +33,4 @@ def send_message(message_, from_, to):
     )'''
 
 
-send_message("Hi there", "+14155238886", "whatsapp:+19142822807")
+#send_message("Hi there", "+14155238886", 'whatsapp:+19142822807")
